9fd0d9bc09034d719c2f74095252dcf9217b421c
RowanGill <rowan.gill@infinityworks.com>
2017-09-27 14:19:11 +0100

    Changed environment variables to not be hard coded

--- testjs.js
+++ testjs.js
@@ -2,19 +2,25 @@ var http = require('http');
 var connect_db = false
 var RETRY_TIME = 1000
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'db',
-  user     : 'root',
-  password : 'example'
-});
+var USER_ID = process.env.USER_ID
+var USER_PW = process.env.USER_PW
+var HOST_DB = process.env.HOST_DB
+var connection
 
 
 
 function connectToDB(){
 	if(connect_db == false){
+    connection = mysql.createConnection({
+      host     : HOST_DB,
+      user     : USER_ID,
+      password : USER_PW
+    });
+    console.log('trying to connect')
 		connection.connect(function(err) {
   			if (err) {
   				setTimeout(connectToDB, RETRY_TIME);
+          console.log(`failed using id: ${USER_ID}, host: ${HOST_DB}, pw: ${USER_PW} : ${err.message}`);
           return;
         }else{
     			connect_db = true
