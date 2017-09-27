b1eb55e858ad69c5c90405e349c0ec148b01958c
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-09-27 15:32:59 +0100

    Changed the node js file to display an image instead of a connection string success/error

--- testjs.js
+++ testjs.js
@@ -1,6 +1,8 @@
 var http = require('http');
-var connect_db = false
-var RETRY_TIME = 1000
+var connected = '<html><img src="https://ak1.picdn.net/shutterstock/videos/646681/thumb/6.jpg" alt="missed the bullseye"></html';
+var notConnected = '<html><img src="http://jewishjournal.com/images/bullseye.jpg" alt="missed the bullseye"></html';
+var connect_db = false;
+var RETRY_TIME = 1000;
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'db',
@@ -17,7 +19,7 @@ function connectToDB(){
   				setTimeout(connectToDB, RETRY_TIME);
           return;
         }else{
-    			connect_db = true
+    			connect_db = true;
           console.log('connected as id ' + connection.threadId);
     		}
     });
@@ -28,12 +30,12 @@ function connectToDB(){
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     if (connect_db == false){
-    	res.end('Can\'t connect to db')
+    	res.end(notConnected);
     }
     else{
-    	res.end('Connected to db')
+    	res.end(connected);
     }
     
 }).listen(8888);
 
-connectToDB()
+connectToDB();
