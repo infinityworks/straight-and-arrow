2e86d99ae42c54141e3a517a6975e40bff9b25c4
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-09-27 14:15:43 +0100

    Changed bin files and travis yaml file

--- bin/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rust-musl-builder() {
-    docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder "$@"
-}
-
-sudo chmod 777 "$(pwd)/target"
-rust-musl-builder cargo build --release
