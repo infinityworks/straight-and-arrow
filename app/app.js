c47f4f5233ec331cb1f6ff55c63235c47608be66
John James <john.james@infinityworks.com>
2017-09-29 12:25:32 +0100

    can now view db query at /users

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const port = 8888;
 
+app.get('/users', require('./usertest'))
+
 //This app-use function will use all the files in the layout folder.
 app.use(express.static(path.join(__dirname, './layouts')))
 
