6c3c818d84a070ffea5b48e52bf9b096d1758b39
John James <john.james@infinityworks.com>
2017-09-29 14:30:04 +0100

    email basic logging

--- app/app.js
+++ app/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 8888;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/users', require('./usertest'))
+app.post('/capture-email', function(req,res){
+    console.log(req.body)
+    res.redirect('/success')
+})
 
 //This app-use function will use all the files in the layout folder.
 app.use(express.static(path.join(__dirname, './layouts')))
