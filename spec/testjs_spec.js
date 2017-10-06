4e6d32c4a28a10bb7a2fb598c60eed62e7a774be
RowanGill <rowan.gill@infinityworks.com>
2017-10-06 09:03:02 +0100

    reverting jasmine files

--- spec/testjs_spec.js
+++ spec/testjs_spec.js
@@ -1,30 +1,7 @@
-'use strict'
-let request = require('request');
-let home_url = "http://localhost:8888/"
+const helloWorld = require('../jasminetests');
 
-
-describe("Home page functions", () => {
-
-
-	it("Returns status code 200", (done) => {
-		request.get(home_url, (error, response, body) => {
-			expect(response.statusCode).toBe(200);
-			done()
-		});
+describe("Hello world", function() {
+	it("returns hello world", function() {
+    expect(helloWorld()).toEqual("Hello World!");
 	});
-
-	it('should not respond with a 404', (done) => {
-        request(home_url, (error, response, body)=> {
-            expect(response.statusCode).not.toBe(404)
-            done()
-        })
-    })
-
-    // is this good, if not why not?
-    it('should have a page title of "Welcome to IWAO"', (done) => {
-        request(home_url, (error, response, body)=> {
-            expect(body).toContain('Welcome to IWAO')
-            done()
-        })
-    });
 });
