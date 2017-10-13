84c4abeb358e9e5d24001385fe940a6e1b4e537b
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-10-13 09:56:22 +0100

    Changed deploy scripts to dev only

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -2,21 +2,7 @@
 # Push only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
-
-    # This is needed to login on AWS and push the image on ECR
-    # Change it accordingly to your docker repo
-    pip install --user awscli
-    export PATH=$PATH:$HOME/.local/bin
-    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
-
-    # Build and push
-    docker build -t $IMAGE_NAME .
-    echo "Pushing $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
-    docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
-    docker push "$REMOTE_IMAGE_URL:latest"
-    echo "Pushed $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
-  elif [ "$TRAVIS_BRANCH" == "dev" ]; then
+  if [ "$TRAVIS_BRANCH" == "dev" ]; then
 
     # This is needed to login on AWS and push the image on ECR
     # Change it accordingly to your docker repo
--- bin/ecs_deploy.sh
+++ bin/ecs_deploy.sh
@@ -2,15 +2,7 @@
 # Deploy only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
-
-    pip install --user awscli
-    export PATH=$PATH:$HOME/.local/bin
-    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
-
-    echo "Deploying $TRAVIS_BUILD_NUMBER on $TASK_DEFINITION"
-    ./bin/ecs-deploy -c $CLUSTER -n service-prod -i $REMOTE_IMAGE_URL:latest
-  elif [ "$TRAVIS_BRANCH" == "dev" ]; then
+  if [ "$TRAVIS_BRANCH" == "dev" ]; then
 
     pip install --user awscli
     export PATH=$PATH:$HOME/.local/bin
