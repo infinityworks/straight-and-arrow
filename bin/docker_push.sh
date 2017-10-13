59bc7c0e47435c9bd4f70609b44ad136b2f89e55
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-10-13 08:37:21 +0100

    Added  dev condtion to if statemetn to target pushing container to ECS

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -16,6 +16,20 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
     docker push "$REMOTE_IMAGE_URL:latest"
     echo "Pushed $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
+  elif [ "$TRAVIS_BRANCH" == "dev" ]; then
+
+    # This is needed to login on AWS and push the image on ECR
+    # Change it accordingly to your docker repo
+    pip install --user awscli
+    export PATH=$PATH:$HOME/.local/bin
+    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
+
+    # Build and push
+    docker build -t $IMAGE_NAME .
+    echo "Pushing $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
+    docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
+    docker push "$REMOTE_IMAGE_URL:latest"
+    echo "Pushed $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
   else
     echo "Skipping deploy because branch is not 'master'"
   fi
