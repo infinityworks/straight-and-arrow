519352553fd5189274d01841322d08b263e57ad6
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-10-19 11:46:41 +0100

    Fixed functionality to return the list of players scores

--- app/controller/tournament-score-controller.js
+++ app/controller/tournament-score-controller.js
@@ -2,58 +2,40 @@ module.exports = (executeQuery, app, tournamentArchers, tournamentScore, tournam
 
    return { showTournamentScore };
 
-
-
-
     function showTournamentScore(req, res) {
 
         const tournamentID = req.params.tid;
         const archerID = req.params.aid;
         let tournamentScores = []
 
-
         tournamentArchers.getTournamentArchers(tournamentID, archerID, (archerIDs) => {
-            console.log("Archer IDs",  archerIDs);
             
             archerIDs.forEach((archerID)=>{
                 tournamentScore.getTournamentScore(tournamentID, archerID.archer_id, (archerData) => {
-                console.log("Archer Data",  archerData);
 
-                tournamentStats.getTournamentStats(tournamentID, (archerStats) => {
+                    tournamentStats.getTournamentStats(tournamentID, archerID.archer_id, (archerStats) => {
 
-                    tournamentScores.push(tabulatedResults(archerData))
-                    console.log("Tournament Scores", tournamentScores)
+                        let archer = []
+                        archer.id = archerID
+                        archer.ends = tabulatedResults(archerData)
+                        archer.summary = archerStats
 
-
-
-                    //stolen from app.js
-                    // let archer = []
-                    // archer.id = archerID
-                    // archer.ends = tabulateResult(singleArcherData)
-                    // archer.summary = singleArcherScore
-                    // tournamentScores.push(archer)
-
-                    // if (archerIDs.length == tournamentScores.length){
-                        app.render('tournament-score.html', {
-                            data: tournamentScores,
-                        }, (err, content) => {
-                            res.render('fullpage.html', {
-                                title: "Archer Score for Tournament",
-                                year: "2017",
-                                content: content
+                        tournamentScores.push(archer)
+                        if (archerIDs.length == tournamentScores.length){
+                            
+                            app.render('tournament-score.html', {
+                                data: tournamentScores,
+                            }, (err, content) => {
+                                res.render('fullpage.html', {
+                                    title: "Archer Score for Tournament",
+                                    year: "2017",
+                                    content: content
+                                })
                             })
-                        })
-                    //}
-                    //stolen from app js
-
-                    
-                    console.log("Archer stats", archerStats);
-
-                })    
-
-            })
-            })        
-            
+                        }
+                    })    
+                })
+            })         
         })    
     }
 }
--- app/data/mGetTournamentScore.js
+++ app/data/mGetTournamentScore.js
@@ -3,7 +3,7 @@ module.exports = (executeQuery) => {
     return { getTournamentScore }
 
     function getTournamentScore(tournamentID, archerID, callback){ 
-		console.log(tournamentID, archerID)
+		// console.log(tournamentID, archerID)
         executeQuery(`SELECT arrow, score, spider
             FROM arrow arr
             WHERE arr.tournament = ? and arr.archer = ?`, 
