74e92c57d6f758669d5f73e24eb7b7022e52d019
RowanGill <rowan.gill@infinityworks.com>
2017-11-03 10:33:10 +0000

    made progress on the functionality of the registration form, now throws an error if they get cheeky

--- app/controller/registration-controller.js
+++ app/controller/registration-controller.js
@@ -1,14 +1,17 @@
 module.exports = (executeQuery, app, utility) => {
 
-    return { showRegistration, sendRegistration, checkEmailsMatch, 
-        checkEmailUnique, checkPasswordsMatch, checkPasswordPolicy , 
-        checkPasswordLength };
+    return {
+        showRegistration,
+        sendRegistration,
+        checkEmailsMatch,
+        checkEmailUnique,
+        checkPasswordsMatch,
+        checkPasswordPolicy,
+        checkPasswordLength
+    };
 
     function showRegistration(req, res) {
-
-
         app.render('registration.html', {
-
         }, (err, content) => {
             res.render('fullpage.html', {
                 title: "Registration",
@@ -18,31 +21,37 @@ module.exports = (executeQuery, app, utility) => {
         })
     }
 
-function sendRegistration(req, res) {
+    function sendRegistration(req, res) {
+        regInput = req.body
+        passwordsMatch = checkPasswordsMatch(regInput.password, regInput.cpassword)
+        emailsMatch = checkEmailsMatch(regInput.email, regInput.cemail)
+        emailUnique = checkEmailUnique(regInput.email)
+        passwordPolicy = checkPasswordPolicy(regInput.password)
+        hashword = regInput.password
 
-       if(checkEmailUnique(req.body.email) == false){
-           console.log("need to do the database count thing here")
-       } else{
-
-            // //password hashing should go here
-            // hashword = regInput.password
-
-            // executeQuery(`INSERT INTO player (name, dob, email, password)
-            //             VALUES (?,?,?,?)`,
-            //             [regInput.name, regInput.dob, regInput.email, hashword],(result) =>{
-
-           app.render('registrationSuccess.html', {
-
-           }, (err, content) => {
-               res.render('fullpage.html', {
-                   title: "Registration Success",
-                   year: "2017",
-                   content: content
-               })
-           })
-       // })
-   }
-}
+        if (!passwordsMatch || !emailsMatch || !passwordPolicy) {
+            throw 403;
+        }
+        if (!emailUnique) {
+            app.render('emailNotUnique.html', {}, (err, content) => {
+                res.render('fullpage.html', {
+                    title: "Archer Score for Tournament",
+                    year: "2017",
+                    content: content
+                })
+            })
+        }
+        executeQuery(`INSERT INTO player (name, dob, email, password)
+        VALUES (?,?,?,?)`, [regInput.name, regInput.dob, regInput.email, hashword], (result) => {
+            app.render('registrationSuccess.html', {}, (err, content) => {
+                res.render('fullpage.html', {
+                    title: "Archer Score for Tournament",
+                    year: "2017",
+                    content: content
+                })
+            })
+        })
+    }
 
 
 
@@ -54,25 +63,21 @@ function sendRegistration(req, res) {
         return utility.checkCredentialsMatch(email, cemail)
     }
 
-    function checkEmailUnique(email){
+    function checkEmailUnique(email) {
         return true
         // select count(*) from table where email = email
         // if 0 it's unique, if 1 not unique
     }
 
-    function checkPasswordPolicy(pass){
+    function checkPasswordPolicy(pass) {
         return checkPasswordLength(pass)
     }
 
-    function checkPasswordLength(password){
-        if (password.length <8 || password.length >20){
+    function checkPasswordLength(password) {
+        if (password.length < 8 || password.length > 20) {
             return false
-        }
-        else {
+        } else {
             return true
         }
     }
-
-    // add DOB check?
-
 }
