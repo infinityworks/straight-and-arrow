0cab8534e297c9155584c1ddaf8ed7b88d84e783
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-11-08 17:29:47 +0000

    Added the input boxes to have score inputs retrieved from database

--- app/controller/prediction-controller.js
+++ app/controller/prediction-controller.js
@@ -4,33 +4,32 @@ module.exports = (executeQuery, app, tournamentArchers, predictions) => {
 
     function showPredictionPage(req, res) {
 
-        let predictionsObject = [{predictionObject1:{}},
-                                {predictionObject2:{}},
-                                {predictionObject3:{}}]
+        let predictionsObject = []
         console.log("the empty predob", predictionsObject)
         const tournamentID = req.params.tid
-        const archerID = 1
         const sessionID = 1
 
-        tournamentArchers.getTournamentArchers(tournamentID, archerID, (archerIDs) => {
-            predictions.getPredictions(sessionID, tournamentID, (playerPrediction) =>{
-                console.log("possible archer id", archerIDs)
-                console.log("original data", playerPrediction)
+        
+        predictions.getPredictions(sessionID, tournamentID, (playerPrediction) =>{
+            
+            playerPrediction.forEach((prediction) => {
+
+                predictionsObject.push({archerPrediction:prediction})
+            })
+            console.log("original data", playerPrediction)
+            console.log("new data", predictionsObject)
 
 
-
-                app.render('prediction.html', {archerIDs, playerPrediction}, (err, content) => {
-                    res.render('fullpage.html', {
-                        title: "Prediction Page",
-                        year: "2017",
-                        content: content
-                    })
+            app.render('prediction.html', {predictionsObject}, (err, content) => {
+                res.render('fullpage.html', {
+                    title: "Prediction Page",
+                    year: "2017",
+                    content: content
                 })
             })
         })
+        
     }
-
-
 
     function sendPredictions(req, res){
 
--- app/data/mGetPredictions.js
+++ app/data/mGetPredictions.js
@@ -3,8 +3,10 @@ module.exports = (executeQuery) => {
     return { getPredictions }
 
     function getPredictions(sessionID, tournamentID, callback) {
-	    executeQuery(`SELECT pred_no, player, tournament, archer, pred_score FROM prediction
-            WHERE tournament = ? AND player = ?`,
+	    executeQuery(`SELECT a.id, a.name, p.player, p.pred_score FROM tournament_archer ta
+		INNER JOIN archer a on ta.archer_id = a.id
+		LEFT OUTER JOIN prediction p on ta.archer_id = p.archer AND p.player = 1 AND p.tournament = 1
+		WHERE ta.predictabool = 1 AND ta.tournament_id = ?`,
 	        [tournamentID, sessionID], (predictions) =>{
 	        	callback(predictions)
 		})
