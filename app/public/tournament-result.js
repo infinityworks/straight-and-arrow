eef05c4aa9565701d0502e2310b172f5a2145abf
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-11-09 17:06:57 +0000

    Added the prediction link to tournament list

--- app/public/tournament-result.js
+++ app/public/tournament-result.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$( "td#Upcoming" ).text( "Upcoming" );
 	$( "td#Live-Result" ).text("Live Results");
+	//$( "td#Make-Prediction" ).replaceWith(`<a href="/predictions`{{id}}`">`{{status}}`</a>`);
 	$( "p#Upcoming" ).text( "Results will become available once the tournament begins");
 	$( "p#Live-Result" ).text("This tournament is currently in progress - results available soon");
 });
