e46a496eccb4809804d7e701d30684b9015659de
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-11-09 09:55:58 +0000

    Added the request body to the cookie

--- app/controller/login-Controller.js
+++ app/controller/login-Controller.js
@@ -22,13 +22,16 @@ module.exports = (executeQuery, app, bcrypt) => {
         email = req.body.email
         password = req.body.password
 
-        executeQuery(`SELECT password FROM player WHERE email = ?`, [email], (result) => {
+        executeQuery(`SELECT id, name, password FROM player WHERE email = ?`, [email], (result) => {
             if(result.length == 0){
                 res.send({status: "email doesn't exist"})
             }
             else {
                 bcrypt.compare(password, result[0].password, (err, match) => {
                     if(match) {
+                        req.session.email = email
+                        req.session.playerID = result[0].id
+                        req.session.name = result[0].name
                         res.redirect("/tournament")
                     }
                     else {
