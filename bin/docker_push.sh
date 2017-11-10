d327cf6ee4b41f49f0de3dab6395b80b9ab5e0c8
RowanGill <rowan.gill@infinityworks.com>
2017-11-10 12:54:32 +0000

    changed docker psuh

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -16,8 +16,19 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
     docker push "$REMOTE_IMAGE_URL:latest"
     echo "Pushed $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
-  else
-    echo "Skipping deploy because branch is not 'master'"
+elif [ "$TRAVIS_BRANCH" == "master" ]; then
+      # This is needed to login on AWS and push the image on ECR
+      # Change it accordingly to your docker repo
+      pip install --user awscli
+      export PATH=$PATH:$HOME/.local/bin
+      eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
+
+      # Build and push
+      docker build -t $IMAGE_NAME .
+      echo "Pushing $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
+      docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:latest"
+      docker push "$REMOTE_IMAGE_URL:latest"
+      echo "Pushed $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
   fi
 else
   echo "Skipping deploy because it's a pull request"
