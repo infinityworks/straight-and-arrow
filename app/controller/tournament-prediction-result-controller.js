7c48aa33a8f8ba3650178a979de1cbaf96c8e955
RowanGill <rowan.gill@infinityworks.com>
2017-11-15 21:53:14 +0000

    added the start of the results page for comparing predictions, mustache needs work

---
+++ app/controller/tournament-prediction-result-controller.js
@@ -0,0 +1,59 @@
+module.exports = (app, allPredictions, archerScoreSum) =>{
+
+    return {showPredictionResults};
+
+    function showPredictionResults(req, res){
+        const tournamentID = req.params.tid
+
+        archerScoreSum.getPredictableArcherScoreSum(tournamentID, (archerScoreSum)=>{
+            allPredictions.getAllPredictions(tournamentID, (allPrediction) =>{
+            playerScore = {}
+            mustachePlayerScore = []
+                for (var i = 0; i < allPrediction.length; i++){
+                    prediction = allPrediction[i]
+                    player_name = prediction.name
+                    player_ID = prediction.id
+                    archer_ID = prediction.archer
+                    prediction = prediction.pred_score
+                    archer_score = getArcherScore(archerScoreSum, archer_ID)
+                    console.log("prediction",prediction)
+                    console.log("archer_score", archer_score)
+                    diff = Math.abs(prediction - archer_score)
+                    console.log(diff)
+                    if (!playerScore[player_ID]){
+                        playerScore[player_ID] = {name:player_name, rankingScore:0}
+                    }
+                    playerScore[player_ID].rankingScore += diff;
+                    console.log("playerScore", playerScore)
+                }
+                for (var key in playerScore){
+                    mustachePlayerScore.push(playerScore[key])
+                }
+
+
+                console.log(mustachePlayerScore)
+                app.render('display-prediction-results.html', {
+                    playerScore
+                }, (err, content) => {
+                    res.render('fullpage.html', {
+                        title: "Archer Scores for Tournament",
+                        year: "2017",
+                        content: content
+                })
+            })
+            })
+        })
+    }
+
+    function getArcherScore(archerScoreSum, archer_ID){
+        console.log("passed archerscoresum", archerScoreSum)
+        console.log("passed archer id", archer_ID)
+        for (var i = 0; i < archerScoreSum.length; i++){
+            row = archerScoreSum[i]
+            if (row.archer == archer_ID){
+                return row.score;
+            }
+        }
+        return null;
+    }
+}
