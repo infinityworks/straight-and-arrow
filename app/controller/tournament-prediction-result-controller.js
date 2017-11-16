86be707a4cd9891e26c067addbef307a84288827
Rebecca Denyer <rebecca.denyer@infinityworks.com>
2017-11-16 15:46:14 +0000

    added logic to sort players by diff and add rank based on this ordered list

--- app/controller/tournament-prediction-result-controller.js
+++ app/controller/tournament-prediction-result-controller.js
@@ -7,33 +7,39 @@ module.exports = (app, allPredictions, archerScoreSum) =>{
 
         archerScoreSum.getPredictableArcherScoreSum(tournamentID, (archerScoreSum)=>{
             allPredictions.getAllPredictions(tournamentID, (allPrediction) =>{
-            playerScore = {}
+            playerScores = []
             mustachePlayerScore = []
                 for (var i = 0; i < allPrediction.length; i++){
+
                     prediction = allPrediction[i]
+
                     player_name = prediction.name
                     player_ID = prediction.id
                     archer_ID = prediction.archer
-                    prediction = prediction.pred_score
+                    predictedScore = prediction.pred_score
                     archer_score = getArcherScore(archerScoreSum, archer_ID)
-                    console.log("prediction",prediction)
-                    console.log("archer_score", archer_score)
-                    diff = Math.abs(prediction - archer_score)
-                    console.log(diff)
-                    if (!playerScore[player_ID]){
-                        playerScore[player_ID] = {name:player_name, rankingScore:0}
+                    diff = Math.abs(predictedScore - archer_score)
+
+                    if (!playerScores[player_ID]){
+                        playerScores[player_ID] = {name:player_name, predictionDiff:0}
                     }
-                    playerScore[player_ID].rankingScore += diff;
-                    console.log("playerScore", playerScore)
+                    playerScores[player_ID].predictionDiff += diff;
                 }
-                for (var key in playerScore){
-                    mustachePlayerScore.push({diff:playerScore[key]})
+
+                playerScores.sort(function(a, b) {
+                return parseFloat(a.predictionDiff) - parseFloat(b.predictionDiff);
+                });
+
+
+                for (var i=0; i < (playerScores.length-1); i++) {
+                    playerScores[i].rank = i+1
+                    playerObject = playerScores[i]
+                    mustachePlayerScore.push({playerInfo: playerObject})
                 }
 
 
-                console.log(mustachePlayerScore)
                 app.render('display-prediction-results.html',
-                    {playerScore: mustachePlayerScore}
+                    {playerScores: mustachePlayerScore}
                 , (err, content) => {
                     res.render('fullpage.html', {
                         title: "Archer Scores for Tournament",
@@ -46,8 +52,6 @@ module.exports = (app, allPredictions, archerScoreSum) =>{
     }
 
     function getArcherScore(archerScoreSum, archer_ID){
-        console.log("passed archerscoresum", archerScoreSum)
-        console.log("passed archer id", archer_ID)
         for (var i = 0; i < archerScoreSum.length; i++){
             row = archerScoreSum[i]
             if (row.archer == archer_ID){
--- app/data/getPredictableArcherScores.js
+++ app/data/getPredictableArcherScores.js
@@ -5,10 +5,10 @@ module.exports = (executeQuery) => {
     function getPredictableArcherScoreSum(tournamentID, callback) {
 	    executeQuery(`SELECT arr.archer, SUM(arr.score) AS score
                     FROM arrow arr
-                    INNER JOIN tournament_archer ta on ta.archer_id = arr.archer AND ta.tournament_id = 4
+                    INNER JOIN tournament_archer ta on ta.archer_id = arr.archer AND ta.tournament_id = ?
                     WHERE arr.tournament = ? AND ta.predictabool = 1
                     GROUP BY arr.archer`,
-	        [tournamentID], (predictions) =>{
+	        [tournamentID, tournamentID], (predictions) =>{
 	        	callback(predictions)
 		})
 	//this should return an array of predictions
